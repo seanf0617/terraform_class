239bfc2b33b995436847d8a2543b0152db0d994f
Sean Fitzgerald <seanf0617@gmail.com>
2026-02-03 11:52:24 -0500

    Added blank lines between local variables

--- day_11/local.tf
+++ day_11/local.tf
@@ -1,12 +1,16 @@
 locals  {
     formatted_project_name = lower(replace(var.project_name, " ", "-"))
+    
     new_tag = merge(var.default_tags,var.environment_tags)
+    
     formatted_bucket_name = replace(
         replace(
         substr(lower(var.bucket_name),0,63),
         " ", "-")
         ,"!", "")
+    
     port_list = split(",", var.allowed_ports)
+    
     sg_rules = [ 
         for port in local.port_list :
         {
